556727f785f3da2508a892b3ae5cc85ec32a2214
rikifujihara <rikifujihara@gmail.com>
2022-06-20 15:33:19 +1000

    cleaned up users routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,10 @@
 Rails.application.routes.draw do
-  get 'users/index'
-  get 'users/show'
-  get 'users/edit'
   get 'orders/success'
   get 'orders/bought'
   get 'orders/sold'
   resources :listings
   devise_for :users
+  resources :users, only: [:index, :show, :edit, :update]
   root 'pages#home'
 
   post 'listings/:id/order', to: 'listings#place_order', as: "place_order"
