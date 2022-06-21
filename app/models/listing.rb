7b62fe4bc79aaf1f67ef9c2154907963d07b6113
rikifujihara <rikifujihara@gmail.com>
2022-06-21 11:29:57 +1000

    user can delete their listing before or after they've been sold. added has_many :orders, dependent: :destroy to listing model

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -2,4 +2,5 @@ class Listing < ApplicationRecord
   belongs_to :user
   has_one_attached :picture, dependent: :destroy
   validates :picture, content_type: [:png, :jpg, :jpeg]
+  has_many :orders, dependent: :destroy
 end
