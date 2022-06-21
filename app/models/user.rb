41a0d2b6edbc62eff904fce08d34b98a4add1802
rikifujihara <rikifujihara@gmail.com>
2022-06-21 10:57:35 +1000

    added card layout to user show page to display their listings

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,4 +11,5 @@ class User < ApplicationRecord
 
   has_many :sold_orders, foreign_key: "seller_id", class_name: "Order"
   has_many :bought_orders, foreign_key: "buyer_id", class_name: "Order"
+
 end
