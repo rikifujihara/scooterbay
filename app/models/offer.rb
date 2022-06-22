93832791d8661f89d2e6779dcbfe358565dfa01b
rikifujihara <rikifujihara@gmail.com>
2022-06-22 14:31:02 +1000

    added relations between offers and users

--- app/models/offer.rb
+++ app/models/offer.rb
@@ -1,5 +1,5 @@
 class Offer < ApplicationRecord
   belongs_to :listing
-  belongs_to :offerer
-  belongs_to :merchant
+  belongs_to :offerer, foreign_key: "offerer_id", class_name: "Offer"
+  belongs_to :merchant, foreign_key: "merchant_id", class_name: "Offer"
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -11,5 +11,7 @@ class User < ApplicationRecord
 
   has_many :sold_orders, foreign_key: "seller_id", class_name: "Order"
   has_many :bought_orders, foreign_key: "buyer_id", class_name: "Order"
+  has_many :offers_in, foreign_key: "merchant_id", class_name: "Offer"
+  has_many :offers_out, foreign_key: "offerer_id", class_name: "Offer"
 
 end
