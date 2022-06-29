3e809ea234064da21a911473d83d2375cf797a3a
rikifujihara <rikifujihara@gmail.com>
2022-06-29 18:38:30 +1000

    client side validation for users mobile numbers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,6 @@ class ApplicationController < ActionController::Base
     protected
   
     def configure_permitted_parameters
-      devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :first_name, :admin])
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :first_name, :admin, :mobile])
     end
 end
