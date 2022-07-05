935991aeb6bb08044f0f0141ada5693add6af319
rikifujihara <rikifujihara@gmail.com>
2022-07-05 12:26:18 +1000

    validate suburb and state on backend

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,5 +1,7 @@
 class Address < ApplicationRecord
   belongs_to :listing
   validates :postcode, allow_nil: true, length: { is: 4 }
-  validates :state, allow_nil: true, length: { is: 3 }, format: {with: /[A-Z]/, message: ": Please use capital letters (eg. VIC)"}
+  validates :state, allow_nil: true, length: { minimum: 2, maximum: 3 }, format: {with: /[A-Z]/, message: ": Please use capital letters (eg. VIC)"}
+  validates :suburb, allow_nil: true, format: {with: /[a-zA-Z]/, message: ": Letters only please"}
+  validates :street_name, allow_nil: true, format: {with: /[a-zA-Z]/, message: ": Letters only please"}
 end
