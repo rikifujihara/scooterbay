76842b520575b48191a10f059f8eac9b01c7889d
rikifujihara <rikifujihara@gmail.com>
2022-07-05 12:29:00 +1000

    numericality validation for street number

--- app/models/address.rb
+++ app/models/address.rb
@@ -4,4 +4,5 @@ class Address < ApplicationRecord
   validates :state, allow_nil: true, length: { minimum: 2, maximum: 3 }, format: {with: /[A-Z]/, message: ": Please use capital letters (eg. VIC)"}
   validates :suburb, allow_nil: true, format: {with: /[a-zA-Z]/, message: ": Letters only please"}
   validates :street_name, allow_nil: true, format: {with: /[a-zA-Z]/, message: ": Letters only please"}
+  validates :street_number, allow_nil: true, numericality: true
 end
