68109583a3a73aefe5e37414bdcaa4f818c77c45
rikifujihara <rikifujihara@gmail.com>
2022-07-05 12:10:16 +1000

    backend validation for address state

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,4 +1,5 @@
 class Address < ApplicationRecord
   belongs_to :listing
   validates :postcode, allow_nil: true, length: { is: 4 }
+  validates :state, allow_nil: true, length: { is: 3 }, format: {with: /[A-Z]/, message: ": Please use capital letters (eg. VIC)"}
 end
