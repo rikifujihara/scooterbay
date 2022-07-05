970e051dc4d5dae7ef2cf820139b9ee4fda79881
rikifujihara <rikifujihara@gmail.com>
2022-07-05 12:47:26 +1000

    adjusted address validation

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,8 +1,8 @@
 class Address < ApplicationRecord
   belongs_to :listing
-  validates :postcode, allow_nil: true, length: { is: 4 }
-  validates :state, allow_nil: true, length: { minimum: 2, maximum: 3 }, format: {with: /[A-Z]/, message: ": Please use capital letters (eg. VIC)"}
-  validates :suburb, allow_nil: true, format: {with: /[a-zA-Z]/, message: ": Letters only please"}
-  validates :street_name, allow_nil: true, format: {with: /[a-zA-Z]/, message: ": Letters only please"}
+  validates :postcode, allow_blank: true, length: { is: 4 }
+  validates :state, allow_blank: true, length: { minimum: 2, maximum: 3 }, format: {with: /[A-Z]*/, message: ": Please use capital letters (eg. VIC)"}
+  validates :suburb, allow_blank: true, format: {with: /[a-zA-Z]+/, message: ": Letters only please"}
+  validates :street_name, allow_blank: true, format: {with: /[a-zA-Z]+/, message: ": Letters only please"}
   validates :street_number, allow_nil: true, numericality: true
 end
