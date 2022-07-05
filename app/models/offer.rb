9fb111c752ecba3a648826afe1e325ea70ac03ee
rikifujihara <rikifujihara@gmail.com>
2022-07-05 12:53:57 +1000

    price validation for offers

--- app/models/offer.rb
+++ app/models/offer.rb
@@ -2,4 +2,5 @@ class Offer < ApplicationRecord
   belongs_to :listing
   belongs_to :offerer, foreign_key: "offerer_id", class_name: "User"
   belongs_to :merchant, foreign_key: "merchant_id", class_name: "User"
+  validates :price, allow_nil: true, numericality: true
 end
