e47c6db3e07409eae951a199c4518a3542007a27
rikifujihara <rikifujihara@gmail.com>
2022-07-06 17:27:21 +1000

    put category cards into partial with great pain

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,10 @@
 class PagesController < ApplicationController
   def home
+    @category_info = [
+      offroad = {title: 'Off-Road', description: 'Scooters built for rough terrain', pic: "https://scooterbay-public-images.s3.ap-southeast-2.amazonaws.com/offroad.jpeg", link: 'listings/offroad'},
+      high_speed = {title: 'High-Speed', description: 'Powerful scooters with high top speeds and acceleration', pic: "https://scooterbay-public-images.s3.ap-southeast-2.amazonaws.com/highspeed.jpg", link: 'listings/highspeed'},
+      kids = {title: 'Kids Scooters', description: 'Smaller scooters suitable for children', pic: "https://scooterbay-public-images.s3.ap-southeast-2.amazonaws.com/kids-scooter.jpeg", link: 'listings/kids'},
+      offroad = {title: 'Commuter', description: 'Scooters for ideal for short-medium trips on paved terrain', pic: "https://scooterbay-public-images.s3.ap-southeast-2.amazonaws.com/commuter.jpg", link: 'listings/commuter'}
+  ]
   end
 end
--- app/models/page.rb
+++ app/models/page.rb
@@ -1,2 +1,8 @@
 class Page < ApplicationRecord
+    @category_info = [
+        offroad = {title: 'Off-Road', description: 'Scooters built for rough terrain', img_link: }
+        high-speed = {title: 'High-Speed', description: 'Powerful scooters with high top speeds and acceleration'}
+        kids = {title: 'Kids Scooters', description: 'Smaller scooters suitable for children'}
+        offroad = {title: 'Commuter', description: 'Scooters for ideal for short-medium trips on paved terrain'}
+    ]
 end
